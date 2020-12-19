3126bb1fbf330374a348287bf6c78dd71c5b7812
Daniel Kingsbury <dpk14@duke.edu>
2020-12-19 18:27:00 -0500

    allowed for topic search in search bar

--- components/screens/base/JournalScreens/ReadScreen.js
+++ components/screens/base/JournalScreens/ReadScreen.js
@@ -19,33 +19,35 @@ let MARGIN_HORIZONTAL = 15
 let MARGIN_BOTTOM = 30
 let BAR_SCALE = .6
 let TOPIC_BANK_SCALE = .75
-let BAR_HEIGHT = getScreenHeight()*.055
+let BAR_HEIGHT = getScreenHeight() * .055
 let JOURNAL_CONTAINER_SCALE = .85
 
 export default class ReadScreen extends Screen {
 
     static DEFAULT_JOURNAL_TITLE = 'All entries:'
-    static BASE_JOURNAL_TITLE = 'All entries about '
+    static BASE_JOURNAL_TITLE = 'All entries about'
 
     constructor(props) {
         super(props);
         this.state = {
-            ...this.state, ...{journalLoading : true,
-                journalSaving : true,
-                loading : false}
+            ...this.state, ...{
+                journalLoading: true,
+                journalSaving: true,
+                loading: false
+            }
         }
     }
 
-    initialize(journal){
+    initialize(journal) {
         return {
-            activeTopics : new Set(),
-            journal : journal,
-            entries : journal.entries,
-            activeEntries : journal.entries,
-            topics : journal.topics,
-            searched : '',
-            journalLoading : false,
-            initializing : true,
+            activeTopics: new Set(),
+            journal: journal,
+            entries: journal.entries,
+            activeEntries: journal.entries,
+            topics: journal.topics,
+            searched: '',
+            journalLoading: false,
+            initializing: true,
         }
     }
 
@@ -54,7 +56,7 @@ export default class ReadScreen extends Screen {
         this._focusUnsubscribe = this.props.navigation.addListener('focus', () => {
             setTimeout(() => this.tryLoad(), 50);
         })
-        this._blurUnsubscribe = this.props.navigation.addListener('blur', () => setTimeout(() => this.setState({journalSaving : true}), 100));
+        this._blurUnsubscribe = this.props.navigation.addListener('blur', () => setTimeout(() => this.setState({journalSaving: true}), 100));
     }
 
     async tryLoad() {
@@ -63,7 +65,7 @@ export default class ReadScreen extends Screen {
             if (saving) {
                 this.tryLoad()
             } else {
-                this.setState({journalSaving : false})
+                this.setState({journalSaving: false})
                 loginAndInitialize((journal) => this.setState(this.initialize(journal)))
             }
         })
@@ -76,53 +78,69 @@ export default class ReadScreen extends Screen {
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         if (this.state.initializing) {
-            this.setState({initializing : false})
+            this.setState({initializing: false})
         }
     }
 
     _onTopicActivityChange = (topic, isActive) => {
         let {entries, activeTopics} = this.state
-        this.setState({activeEntries : this._getEntriesWithTopics(entries, activeTopics)})
+        this.setState({activeEntries: this._getEntriesWithTopics(entries, activeTopics)})
     }
 
     _getEntriesWithTopics = (entries, activeTopics) => {
         let newActiveEntries = new Set();
-        for(let entry of entries){
+        for (let entry of entries) {
             let hasAllTopics = true;
-            for(let topic of activeTopics) {
-                if (!entry.topics.has(topic)){
+            for (let topic of activeTopics) {
+                if (!entry.topics.has(topic)) {
                     hasAllTopics = false;
                     break;
                 }
             }
-            if(hasAllTopics) newActiveEntries.add(entry)
+            if (hasAllTopics) newActiveEntries.add(entry)
         }
         return newActiveEntries
     }
 
-    _onEntryRemoval = (entry) =>
-    {
+    _onEntryRemoval = (entry) => {
         let {entries, activeEntries} = this.state
         entries.delete(entry)
         activeEntries.delete(entry)
-        this.setState({activeEntries : activeEntries, entries : entries})
+        this.setState({activeEntries: activeEntries, entries: entries})
     }
 
     _getJournalTitle = () => {
-        let {activeTopics} = this.state
-        if (activeTopics.size == 0) return ReadScreen.DEFAULT_JOURNAL_TITLE
+        let {activeTopics, searched} = this.state
+        //if (activeTopics.size == 0 ) return ReadScreen.DEFAULT_JOURNAL_TITLE
         let journalTitle = ReadScreen.BASE_JOURNAL_TITLE
         let activeTopicsArray = Array.from(activeTopics)
-        if (activeTopicsArray.length == 1) {
-            return journalTitle + activeTopicsArray[0] + ":";
+        if (activeTopicsArray.length == 0) {
+            if (searched.length == 0) {
+                return ReadScreen.DEFAULT_JOURNAL_TITLE
+            } else {
+               return journalTitle + " " + searched + ":";
+            }
+        } else if (activeTopicsArray.length == 1) {
+            journalTitle += " " + activeTopicsArray[0];
+            if (searched.length > 0) {
+                journalTitle += " and " + searched
+            }
+            return journalTitle + ":"
         } else if (activeTopicsArray.length == 2) {
-            return journalTitle + activeTopicsArray[0] + " and " + activeTopicsArray[1] + ":";
+            if (searched.length == 0) {
+                return journalTitle + " " + activeTopicsArray[0] + " and " + activeTopicsArray[1] + ":";
+            } else {
+                return journalTitle + " " + activeTopicsArray[0] + ", " + activeTopicsArray[1] + ", and " + searched + ":";
+            }
         } else {
+            if (searched.length > 0) {
+                journalTitle += " " + searched + ","
+            }
             for (let i = 0; i < activeTopicsArray.length; i++) {
                 if (i == (activeTopicsArray.length - 1)) {
-                    return journalTitle + "and " + activeTopicsArray[i] + ":";
+                    return journalTitle + " and " + activeTopicsArray[i] + ":";
                 } else {
-                    journalTitle +=  activeTopicsArray[i] + ", "
+                    journalTitle += " " + activeTopicsArray[i] + ","
                 }
             }
         }
@@ -133,23 +151,26 @@ export default class ReadScreen extends Screen {
         let {entries, activeTopics} = this.state
         let activeSet = this._getEntriesWithTopics(entries, activeTopics)
         let newActiveSet = new Set()
-        for(let entry of activeSet){
-            if (entry.text.includes(searched) || entry.title.includes(searched)) {
+        let searchedCaseInsens = searched.toLowerCase()
+        for (let entry of activeSet) {
+            if (entry.text.toLowerCase().includes(searchedCaseInsens) ||
+                entry.title.toLowerCase().includes(searchedCaseInsens) ||
+                entry.topics.has(searchedCaseInsens) ||
+                entry.topics.has(searched)) {
                 newActiveSet.add(entry)
             }
         }
-        this.setState({activeEntries : newActiveSet})
+        this.setState({activeEntries: newActiveSet})
     }
 
-    getJournalContainerHeight(){
+    getJournalContainerHeight() {
         return (getScreenHeight() -
             HEADER_HEIGHT -
             MARGIN_BOTTOM -
-            (TOPIC_BANK_SCALE * 1.5*TOPIC_BOX_HEIGHT) -
+            (TOPIC_BANK_SCALE * 1.5 * TOPIC_BOX_HEIGHT) -
             (BAR_HEIGHT * BAR_SCALE)
-            -(6*ENTRY_BOX_VERT_MARGIN)
+            - (6 * ENTRY_BOX_VERT_MARGIN)
         )
-
     }
 
     render() {
@@ -157,49 +178,49 @@ export default class ReadScreen extends Screen {
         let journalTitle = this._getJournalTitle()
         let {navigation} = this.props
         let {journal, topics, activeTopics, entries, activeEntries, searched, journalLoading} = this.state
-        return(
+        return (
             <StyledBase>
-                <View style = {[readStyles.outerFrame]}>
+                <View style={[readStyles.outerFrame]}>
                     <SearchBar attrName={'searched'}
-                               value = {searched}
-                               title = {'Search'}
-                               width = {'100%'}
+                               value={searched}
+                               title={'Search'}
+                               width={'100%'}
                                updateMasterState={this._updateMasterState}
-                               style = {{height : BAR_HEIGHT}}
-                               scale = {BAR_SCALE}
+                               style={{height: BAR_HEIGHT}}
+                               scale={BAR_SCALE}
                                onChangeText={this._search}
                     />
                     <JournalContainerBox
-                            title = {journalTitle}
-                            updateMasterState = {this._updateMasterState}
-                            scale = {JOURNAL_CONTAINER_SCALE}
-                            width = {'100%'}
-                            style = {{height : this.getJournalContainerHeight()}}
-                            blurOnSubmit = {false}
-                            active = {entries.size > 0}
-                            entries = {activeEntries}
-                            journal = {journal}
-                            navigation = {navigation}
-                            onEntryRemoval = {this._onEntryRemoval}
-                            value = {''}
-                            />
-                        <TopicBank
-                            attrName = 'topicBank'
-                            setName = 'topicBankCurr'
-                            title = 'Look up your thoughts in the Thinktionary!'
-                            active = {topics.size > 0}
-                            updateMasterState={this._updateMasterState}
-                            scale = {TOPIC_BANK_SCALE}
-                            topicScale = {.62}
-                            topics = {topics}
-                            width = '100%'
-                            height = {1.5*TOPIC_BOX_HEIGHT}
-                            onTopicActivityChange = {this._onTopicActivityChange}
-                            activeTopicsName = {'activeTopics'}
-                            activeTopics = {activeTopics}
-                            value = {''}
-                            reset = {this.state.initializing}
-                        />
+                        title={journalTitle}
+                        updateMasterState={this._updateMasterState}
+                        scale={JOURNAL_CONTAINER_SCALE}
+                        width={'100%'}
+                        style={{height: this.getJournalContainerHeight()}}
+                        blurOnSubmit={false}
+                        active={entries.size > 0}
+                        entries={activeEntries}
+                        journal={journal}
+                        navigation={navigation}
+                        onEntryRemoval={this._onEntryRemoval}
+                        value={''}
+                    />
+                    <TopicBank
+                        attrName='topicBank'
+                        setName='topicBankCurr'
+                        title='Look up your thoughts in the Thinktionary!'
+                        active={topics.size > 0}
+                        updateMasterState={this._updateMasterState}
+                        scale={TOPIC_BANK_SCALE}
+                        topicScale={.62}
+                        topics={topics}
+                        width='100%'
+                        height={1.5 * TOPIC_BOX_HEIGHT}
+                        onTopicActivityChange={this._onTopicActivityChange}
+                        activeTopicsName={'activeTopics'}
+                        activeTopics={activeTopics}
+                        value={''}
+                        reset={this.state.initializing}
+                    />
                 </View>
             </StyledBase>
         )
@@ -207,19 +228,17 @@ export default class ReadScreen extends Screen {
 }
 
 export const readStyles = StyleSheet.create({
-    bottomFrame:{
-        flex : .15,
+    bottomFrame: {
+        flex: .15,
     },
-    leftFrame: {
-    },
-    rightFrame: {
-    },
-    outerFrame:{
-        marginTop : HEADER_HEIGHT,
-        height : getScreenHeight() - HEADER_HEIGHT - MARGIN_BOTTOM,
-        width : getScreenWidth() - 2*MARGIN_HORIZONTAL,
-        marginHorizontal : MARGIN_HORIZONTAL,
-        marginBottom : MARGIN_BOTTOM,
+    leftFrame: {},
+    rightFrame: {},
+    outerFrame: {
+        marginTop: HEADER_HEIGHT,
+        height: getScreenHeight() - HEADER_HEIGHT - MARGIN_BOTTOM,
+        width: getScreenWidth() - 2 * MARGIN_HORIZONTAL,
+        marginHorizontal: MARGIN_HORIZONTAL,
+        marginBottom: MARGIN_BOTTOM,
     }
 
 });
